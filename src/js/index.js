92f88d48c31c1615d78e8241d6a6d49fca3d1275
Brandon Lubbehusen <blubbehusen@yahoo.com>
2020-05-22 15:54:14 +0200

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,13 @@
 'use strict';
 
 const TILE_SIZE = 85;
-
-// let xpos = 685;
-// let ypos = 400;
-
-// document.addEventListener('keydown', (event) => {
-//   const pacFace = document.querySelector('.pacboy-active-dark');
-//   if(event.code === 'ArrowRight') {
-//     pacFace.style.backgroundPositionY = '0%';
-//     xpos += TILE_SIZE
-//     pacFace.style.left = `${xpos}px`;
-//     if (pacFace.style.backgroundPositionX === '100%') {
-//       pacFace.style.backgroundPositionX = '0%';
-//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-//       pacFace.style.backgroundPositionX = '100%';
-//     }
-//   } else if (event.code === 'ArrowLeft') {
-//     pacFace.style.backgroundPositionY = '33%';
-//     xpos -= TILE_SIZE
-//     pacFace.style.left = `${xpos}px`;
-//     if (pacFace.style.backgroundPositionX === '100%') {
-//       pacFace.style.backgroundPositionX = '0%';
-//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-//       pacFace.style.backgroundPositionX = '100%';
-//     }
-//   } else if (event.code === 'ArrowDown') {
-//     pacFace.style.backgroundPositionY = '66%';
-//     ypos -= TILE_SIZE
-//     pacFace.style.bottom = `${ypos}px`;
-//     if (pacFace.style.backgroundPositionX === '100%') {
-//       pacFace.style.backgroundPositionX = '0%';
-//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-//       pacFace.style.backgroundPositionX = '100%';
-//     }
-//   } else if (event.code === 'ArrowUp') {
-//     pacFace.style.backgroundPositionY = '100%';
-//     ypos += TILE_SIZE
-//     pacFace.style.bottom = `${ypos}px`;
-//     if (pacFace.style.backgroundPositionX === '100%') {
-//       pacFace.style.backgroundPositionX = '0%';
-//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-//       pacFace.style.backgroundPositionX = '100%';
-//     }
-//   } 
-// });
 const app = document.querySelector('#app')
+const stage = new Stage(10, 5);
+stage.mount(app);
+const stageElm = document.querySelector('.stage');
 
-const pac1 = new Pacman (600, 400, true);
-pac1.mount(app)
+const pac1 = new Pacman (0, 3, true);
+pac1.mount(stageElm);
 
 document.addEventListener('keydown', (event) => {
 if(event.code === 'ArrowRight') {
@@ -65,4 +24,5 @@ if(event.code === 'ArrowRight') {
   pac1.update('up');
 }
 });
+
 
