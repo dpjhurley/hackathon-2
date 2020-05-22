24b735f7af3a1f0dfa4c878bab9c2339b0f6bc3a
dpjhurley <dpjhurley@gmail.com>
2020-05-22 14:50:41 +0200

    mile2/stage2

--- src/js/pacman-component.js
+++ src/js/pacman-component.js
@@ -12,31 +12,6 @@ class Pacman{
     entityElm.className = 'entity entity--pac';
     entityElm.classList.add('pacboy-active-light');
 
-    this.update(entityElm);
-    document.addEventListener('keydown', (event) => {
-      if(event.code === 'ArrowRight') {
-        entityElm.style.backgroundPositionY = '0%';
-        this.xpos += TILE_SIZE;
-        entityElm.style.left = `${this.xpos}px`;
-        this.move(entityElm)
-      } else if (event.code === 'ArrowLeft') {
-        entityElm.style.backgroundPositionY = '33%';
-        this.xpos -= TILE_SIZE;
-        entityElm.style.left = `${this.xpos}px`;
-        this.move(entityElm);
-      } else if (event.code === 'ArrowDown') {
-        entityElm.style.backgroundPositionY = '66%';
-        this.ypos += TILE_SIZE;
-        entityElm.style.top = `${this.ypos}px`;
-        this.move(entityElm);
-      } else if (event.code === 'ArrowUp') {
-        entityElm.style.backgroundPositionY = '100%';
-        this.ypos -= TILE_SIZE;
-        entityElm.style.top = `${this.ypos}px`;
-        this.move(entityElm);
-      }
-    });
-
     return entityElm;
   }
 
@@ -46,21 +21,41 @@ class Pacman{
     this.update();
   }
 
-  update(entityElm) {
+  update() {
+    this.element.style.left = `${pac1.xpos}px`;
+    this.element.style.top = `${pac1.ypos}px`;
+    if (this.direction === 'right') {
+        this.element.style.backgroundPositionY = '0%';
+    } else if (this.direction === 'left') {
+        this.element.style.backgroundPositionY = '33%';
+    } else if (this.direction === 'down') {
+        this.element.style.backgroundPositionY = '66%';
+    } else if (this.direction === 'up') {
+        this.element.style.backgroundPositionY = '100%';
+    }
     if (this.mouth === true) {
-    entityElm.style.backgroundPositionX = '0%';
+    this.element.style.backgroundPositionX = '0%';
     } else if (this.mouth === false) {
-    entityElm.style.backgroundPositionX = '100%';
+    this.element.style.backgroundPositionX = '100%';
     }
     
   }
 
-  move(entityElm) {
+  move(direction) {
+    if (direction === 'right') {
+        this.xpos += TILE_SIZE;
+    } else if (direction === 'left') {
+        this.xpos -= TILE_SIZE;
+    } else if (direction === 'down') {
+        this.xpos += TILE_SIZE;
+    } else if (direction === 'up') {
+        this.xpos -= TILE_SIZE;
+    }
     if (this.mouth === true) {
-    entityElm.style.backgroundPositionX = '100%';
+    this.element.style.backgroundPositionX = '100%';
     this.mouth = false;
     } else if (this.mouth === false) {
-    entityElm.style.backgroundPositionX = '0%';
+    this.element.style.backgroundPositionX = '0%';
     this.mouth = true;
     }
   }   
