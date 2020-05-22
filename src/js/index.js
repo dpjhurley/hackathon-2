198725a71eb906d503af9e5263d22fa0d98c113f
Brandon Lubbehusen <blubbehusen@yahoo.com>
2020-05-22 12:02:49 +0200

    lunch

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,28 @@ document.addEventListener('keydown', (event) => {
   }
 });
  //changed from left/right to percentage for chrome
+class Pacman{
+    constructor(xpos, mouth) {
+        this.xpos = xpos;
+        this.mouth = mouth;
+    }
+
+    moveRight() {
+        const pacFace = document.querySelector('.pacboy-active-dark');
+        xpos += TILE_SIZE
+        pacFace.style.left = `${xpos}px`;
+        if (pacFace.style.backgroundPositionX === '100%') {
+          pacFace.style.backgroundPositionX = '0%';
+        } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
+          pacFace.style.backgroundPositionX = '100%';
+        }
+    }
+    update() {
+        document.addEventListener('keydown', (event) => {
+            if(event.code === 'ArrowRight') {
+                moveRight()
+            }
+        });
+    }
+}
+
