3a35b9317d52381550c236512552549fd31a58e3
dpjhurley <dpjhurley@gmail.com>
2020-05-22 14:19:41 +0200

    added to pacman component

--- src/js/index.js
+++ src/js/index.js
@@ -2,60 +2,52 @@
 
 const TILE_SIZE = 85;
 
-let xpos = 685;
-let ypos = 400;
+// let xpos = 685;
+// let ypos = 400;
 
-document.addEventListener('keydown', (event) => {
-  const pacFace = document.querySelector('.pacboy-active-dark');
-  if(event.code === 'ArrowRight') {
-    pacFace.style.backgroundPositionY = '0%';
-    xpos += TILE_SIZE
-    pacFace.style.left = `${xpos}px`;
-    if (pacFace.style.backgroundPositionX === '100%') {
-      pacFace.style.backgroundPositionX = '0%';
-    } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-      pacFace.style.backgroundPositionX = '100%';
-    }
-  } else if (event.code === 'ArrowLeft') {
-    pacFace.style.backgroundPositionY = '33%';
-    xpos -= TILE_SIZE
-    pacFace.style.left = `${xpos}px`;
-    if (pacFace.style.backgroundPositionX === '100%') {
-      pacFace.style.backgroundPositionX = '0%';
-    } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-      pacFace.style.backgroundPositionX = '100%';
-    }
-  } else if (event.code === 'ArrowDown') {
-    pacFace.style.backgroundPositionY = '66%';
-    ypos -= TILE_SIZE
-    pacFace.style.bottom = `${ypos}px`;
-    if (pacFace.style.backgroundPositionX === '100%') {
-      pacFace.style.backgroundPositionX = '0%';
-    } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-      pacFace.style.backgroundPositionX = '100%';
-    }
-  } else if (event.code === 'ArrowUp') {
-    pacFace.style.backgroundPositionY = '100%';
-    ypos += TILE_SIZE
-    pacFace.style.bottom = `${ypos}px`;
-    if (pacFace.style.backgroundPositionX === '100%') {
-      pacFace.style.backgroundPositionX = '0%';
-    } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-      pacFace.style.backgroundPositionX = '100%';
-    }
-  } 
-    
-    // xpos += TILE_SIZE
-    // pacFace.style.left = `${xpos}px`;
-    // if (pacFace.style.backgroundPositionX === '100%') {
-    //   pacFace.style.backgroundPositionX = '0%';
-    // } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
-    //   pacFace.style.backgroundPositionX = '100%';
-    // }
-});
-//  changed from left/right to percentage for chrome
+// document.addEventListener('keydown', (event) => {
+//   const pacFace = document.querySelector('.pacboy-active-dark');
+//   if(event.code === 'ArrowRight') {
+//     pacFace.style.backgroundPositionY = '0%';
+//     xpos += TILE_SIZE
+//     pacFace.style.left = `${xpos}px`;
+//     if (pacFace.style.backgroundPositionX === '100%') {
+//       pacFace.style.backgroundPositionX = '0%';
+//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
+//       pacFace.style.backgroundPositionX = '100%';
+//     }
+//   } else if (event.code === 'ArrowLeft') {
+//     pacFace.style.backgroundPositionY = '33%';
+//     xpos -= TILE_SIZE
+//     pacFace.style.left = `${xpos}px`;
+//     if (pacFace.style.backgroundPositionX === '100%') {
+//       pacFace.style.backgroundPositionX = '0%';
+//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
+//       pacFace.style.backgroundPositionX = '100%';
+//     }
+//   } else if (event.code === 'ArrowDown') {
+//     pacFace.style.backgroundPositionY = '66%';
+//     ypos -= TILE_SIZE
+//     pacFace.style.bottom = `${ypos}px`;
+//     if (pacFace.style.backgroundPositionX === '100%') {
+//       pacFace.style.backgroundPositionX = '0%';
+//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
+//       pacFace.style.backgroundPositionX = '100%';
+//     }
+//   } else if (event.code === 'ArrowUp') {
+//     pacFace.style.backgroundPositionY = '100%';
+//     ypos += TILE_SIZE
+//     pacFace.style.bottom = `${ypos}px`;
+//     if (pacFace.style.backgroundPositionX === '100%') {
+//       pacFace.style.backgroundPositionX = '0%';
+//     } else if (pacFace.style.backgroundPositionX === '0%' || pacFace.style.backgroundPositionX === '') {
+//       pacFace.style.backgroundPositionX = '100%';
+//     }
+//   } 
+// });
 
-// const app = document.querySelector('#app')
 
-// const pac1 = new Pacman (600, false);
-// pac1.mount(app)
+const app = document.querySelector('#app')
+
+const pac1 = new Pacman (600, 400, true);
+pac1.mount(app)
--- src/js/pacman-component.js
+++ src/js/pacman-component.js
@@ -1,52 +1,69 @@
 'use strict';
 
 class Pacman{
-    constructor(xpos, mouth) {
-        this.xpos = xpos;
-        this.mouth = mouth;
-    }
+  constructor(xpos, ypos ,mouth) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.mouth = mouth;
+  }
 
-    render() {
-        const entityElm = document.createElement('div');
-        entityElm.className = 'entity entity--pac';
-        entityElm.classList.add('pacboy-active-light');
+  render() {
+    const entityElm = document.createElement('div');
+    entityElm.className = 'entity entity--pac';
+    entityElm.classList.add('pacboy-active-light');
 
-        this.update(entityElm);
-
-        return entityElm;
-    }
-
-    mount(parent) {
-      this.element = this.render();
-      parent.appendChild(this.element);
-      this.update();
-    }
-
-    update(entityElm) {
-      if (this.mouth === true) {
-        entityElm.style.backgroundPositionX = '0%';
-      } else if (this.mouth === false) {
-        entityElm.style.backgroundPositionX = '100%';
-      }
-      document.addEventListener('keydown', (event) => {
-          if(event.code === 'ArrowRight') {
-              this.moveRight(entityElm)
-          }
-      });
-    }
-
-    moveRight(entityElm) {
+    this.update(entityElm);
+    document.addEventListener('keydown', (event) => {
+      if(event.code === 'ArrowRight') {
+        entityElm.style.backgroundPositionY = '0%';
         this.xpos += TILE_SIZE;
         entityElm.style.left = `${this.xpos}px`;
-        if (this.mouth === true) {
-          entityElm.style.backgroundPositionX = '100%';
-          this.mouth = false;
-        } else if (this.mouth === false) {
-          entityElm.style.backgroundPositionX = '0%';
-          this.mouth = true;
-        }
+        this.move(entityElm)
+      } else if (event.code === 'ArrowLeft') {
+        entityElm.style.backgroundPositionY = '33%';
+        this.xpos -= TILE_SIZE;
+        entityElm.style.left = `${this.xpos}px`;
+        this.move(entityElm);
+      } else if (event.code === 'ArrowDown') {
+        entityElm.style.backgroundPositionY = '66%';
+        this.ypos += TILE_SIZE;
+        entityElm.style.top = `${this.ypos}px`;
+        this.move(entityElm);
+      } else if (event.code === 'ArrowUp') {
+        entityElm.style.backgroundPositionY = '100%';
+        this.ypos -= TILE_SIZE;
+        entityElm.style.top = `${this.ypos}px`;
+        this.move(entityElm);
+      }
+    });
+
+    return entityElm;
+  }
+
+  mount(parent) {
+    this.element = this.render();
+    parent.appendChild(this.element);
+    this.update();
+  }
+
+  update(entityElm) {
+    if (this.mouth === true) {
+    entityElm.style.backgroundPositionX = '0%';
+    } else if (this.mouth === false) {
+    entityElm.style.backgroundPositionX = '100%';
     }
     
+  }
+
+  move(entityElm) {
+    if (this.mouth === true) {
+    entityElm.style.backgroundPositionX = '100%';
+    this.mouth = false;
+    } else if (this.mouth === false) {
+    entityElm.style.backgroundPositionX = '0%';
+    this.mouth = true;
+    }
+  }   
 }
 
 
